0e095132865cda4997efdee112af503bd592a5c7
eclipse907 <56761410+eclipse907@users.noreply.github.com>
2021-06-10 14:12:12 +0200

    Changed reading from input stream to read one line at a time

--- GlobalLogicTask/src/main/java/globalLogic/task/CheckCharacterFrequencyInText.java
+++ GlobalLogicTask/src/main/java/globalLogic/task/CheckCharacterFrequencyInText.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.stream.Collectors;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -32,23 +31,22 @@ public class CheckCharacterFrequencyInText implements ICheckCharacterFrequencyIn
 		if (br == null || bw == null) {
 			throw new NullPointerException("Input and output streams can't be null");
 		}
-		String text = br.lines().collect(Collectors.joining(" "));
-		Result result = calculateCharFrequency(text);
-		result.getCharsWordGroupingFrequency().entrySet()
-					   .stream()
-					   .sorted(Comparator.comparingDouble(e -> e.getValue().doubleValue()))
-					   .forEach(e -> {						   
-						   try {
-								bw.write("{" + e.getKey().getCharsToCheckInWord() + ", " + e.getKey().getWordSize() + "} = " + e.getValue() + "\n");
-						   } catch (IOException ex) {
-								System.out.println("Error while writing result to output stream:\n" + ex.getLocalizedMessage());
-								System.exit(1);
-						   }
-					   });
 		try {
+			Result result = calculateCharFrequency(br);
+			result.getCharsWordGroupingFrequency().entrySet()
+			   .stream()
+			   .sorted(Comparator.comparingDouble(e -> e.getValue().doubleValue()))
+			   .forEach(e -> {
+				try {
+					bw.write("{" + e.getKey().getCharsToCheckInWord() + ", " + e.getKey().getWordSize() + "} = " + e.getValue() + "\n");
+				} catch (IOException ex) {
+					System.out.println("Error while writing result to output stream:\n" + ex.getLocalizedMessage());
+					System.exit(1);
+				}
+			});
 			bw.write("TOTAL Frequency: " + result.getTotalCharFrequency() + "\n");
 		} catch (IOException ex) {
-			System.out.println("Error while writing result to output stream:\n" + ex.getLocalizedMessage());
+			System.out.println("Error while reading from input stream or writing to output stream:\n" + ex.getLocalizedMessage());
 			System.exit(1);
 		}
 	}
@@ -63,31 +61,36 @@ public class CheckCharacterFrequencyInText implements ICheckCharacterFrequencyIn
 		return excludedCharacters;
 	}
 	
-	private Result calculateCharFrequency(String text) {
-		String[] wordsInText = text.split("[\\s\\n]+");
+	private Result calculateCharFrequency(BufferedReader inputTextReader) throws IOException {
 		Map<CharactersWordSizeGrouping, Integer> charNumInText = new HashMap<>();
 		int totalNumOfCharsToCheckInText = 0;
 		int totalNumOfCharsInText = 0;
-		for (String word : wordsInText) {
-			int wordSize = 0;
-			int numOfCharsToCheckInWord = 0;
-			List<Character> charsToCheckInWord = new ArrayList<>();
-			for (char c : word.toCharArray()) {
-				if (excludedCharacters.isCharacterExcluded(c)) {
-					continue;
-				}
-				wordSize++;
-				totalNumOfCharsInText++;
-				if (charactersToCheck.isCharacterInWord(c)) {
-					totalNumOfCharsToCheckInText++;
-					numOfCharsToCheckInWord++;
-					charsToCheckInWord.add(Character.toLowerCase(c));
-				}
+		while (true) {
+			String line = inputTextReader.readLine();
+			if (line == null) {
+				break;
 			}
-			if (numOfCharsToCheckInWord > 0) {
-				charsToCheckInWord.sort(charactersToCheck.getWordCharacterComparator());
-				CharactersWordSizeGrouping charNumInWord = new CharactersWordSizeGrouping(new LinkedHashSet<>(charsToCheckInWord), wordSize);
-				charNumInText.put(charNumInWord, charNumInText.getOrDefault(charNumInWord, 0) + numOfCharsToCheckInWord);
+			for (String word : line.split("[\\s]+")) {
+				int wordSize = 0;
+				int numOfCharsToCheckInWord = 0;
+				List<Character> charsToCheckInWord = new ArrayList<>();
+				for (char c : word.toCharArray()) {
+					if (excludedCharacters.isCharacterExcluded(c)) {
+						continue;
+					}
+					wordSize++;
+					totalNumOfCharsInText++;
+					if (charactersToCheck.isCharacterInWord(c)) {
+						totalNumOfCharsToCheckInText++;
+						numOfCharsToCheckInWord++;
+						charsToCheckInWord.add(Character.toLowerCase(c));
+					}
+				}
+				if (numOfCharsToCheckInWord > 0) {
+					charsToCheckInWord.sort(charactersToCheck.getWordCharacterComparator());
+					CharactersWordSizeGrouping charNumInWord = new CharactersWordSizeGrouping(new LinkedHashSet<>(charsToCheckInWord), wordSize);
+					charNumInText.put(charNumInWord, charNumInText.getOrDefault(charNumInWord, 0) + numOfCharsToCheckInWord);
+				}
 			}
 		}
 		Map<CharactersWordSizeGrouping, BigDecimal> charFrequency = new HashMap<>();
